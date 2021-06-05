a7ca1638278dbd0334b39a8ef920b7f0df2c2726
sameer-shaikh <sameermubeen4@gmail.com>
2021-06-05 20:31:42 +0500

    done

--- client/src/screens/CourseDetail/CourseDetail.jsx
+++ client/src/screens/CourseDetail/CourseDetail.jsx
@@ -19,7 +19,41 @@ export default function CourseDetail(props) {
 
 	return (
 		<Layout>
-			<div className="course-details">
+			<div className="mainDivSignUp">
+				<div className="signUpContainer">
+					<div className="container-fluid">
+						<div className="row">
+							<div className="col-12 col-md-6 py-3">
+								<h3 className="fw-bold">Data Structure</h3>
+								<p>
+									Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+									Commodi totam in ratione vitae quia, ab quae magni quas harum
+									enim. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Autem voluptatum eveniet fugiat cumque ipsum vel accusantium
+									dignissimos incidunt soluta laboriosam. Lorem ipsum dolor sit
+									amet, consectetur adipisicing elit. Eius aut totam eligendi
+									repellendus odit vero, aliquid sequi molestias. Placeat, sunt.
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+									quod vel nisi vitae? Laudantium ratione incidunt maxime labore
+									vel ad. Obcaecati repellat saepe qui, asperiores corrupti vel
+									laboriosam molestiae ab?
+								</p>
+								<h6 className="fw-bold float-end">Computer Sciene</h6>
+							</div>
+							<div className="imgContainer col-12 col-md-6 pe-0 ps-0 ps-md-3">
+								<img
+									width="100%"
+									height="100%"
+									src="https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/curious/uploads/pictures/1512133966-1.jpg"
+									alt=""
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* <div className="course-details">
 				{course && (
 					<div className="card">
 						<h3> {course.name}</h3>
@@ -47,7 +81,7 @@ export default function CourseDetail(props) {
 						<Lessons allLessons={course.lessons} />
 					</div>
 				)}
-			</div>
+			</div> */}
 		</Layout>
 	);
 }
